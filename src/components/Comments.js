33d2fc162059df43dae316f5f09b8524cd105b78
markj82 <markjohn666@gmail.com>
2019-07-19 11:17:17 +0100

    fixed hard coded state

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -5,25 +5,8 @@ import '../styles/Comments.css'
 
 class Comments extends React.Component {
     state = {
-        comments: [
-            {
-                comment_id: 115,
-                author: "happyamy2016",
-                article_id: 33,
-                votes: 12,
-                created_at: "2018-01-19T14:47:14.514Z",
-                body: "Neque dolor sint illum id consequuntur debitis qui nam eum. Nam adipisci similique consequatur officiis. Totam qui enim at iste dolorem ullam. Tenetur laudantium sed facilis aspernatur occaecati. Provident rerum quia consectetur et. Molestiae eligendi commodi."
-                },
-                {
-                comment_id: 272,
-                author: "tickle122",
-                article_id: 33,
-                votes: 17,
-                created_at: "2017-09-26T21:34:42.072Z",
-                body: "Distinctio excepturi laboriosam eos aperiam quis amet eum animi minima. Officiis in quia. Est consequatur optio atque nostrum iusto impedit harum quod asperiores."
-                }
-            ]
-        }
+        comments: null
+    }
 
     deleteCommentHandler = commentId => {
 
@@ -41,31 +24,34 @@ class Comments extends React.Component {
     render() { 
         const { user } = this.props
         // user.username for username
-        const commentsToShow = this.state.comments.map(comment => {
-            return (
-                <div key={comment.comment_id}>
-                    
-                    <p className="comment-body">{comment.body}</p>
-                    <p>votes: {comment.votes}</p>
-
-                    {(user === "" ? <p>Only logged in users can vote and post comments</p> :
-                    <>
-                        <button onClick={()=> this.handleVoteUp(comment.comment_id)}>🔼</button>
-                        <button onClick={()=> this.handleVoteDown(comment.comment_id)}>🔽</button>
-                    </>
-                    )}
-                    {(user.username === comment.author ?
-                        <button className="delete-button" onClick={() =>this.deleteCommentHandler(comment.comment_id)}>❌</button> : '')}
-
-                    <hr></hr>
-                </div>
-            )
-        })
+        let commentsToShow
+        if (this.state.comments) {
+            commentsToShow = this.state.comments.map(comment => {
+                return (
+                    <div key={comment.comment_id}>
+                        
+                        <p className="comment-body">{comment.body}</p>
+                        <p>votes: {comment.votes}</p>
+    
+                        {(user === "" ? <p>Only logged in users can vote and post comments</p> :
+                        <>
+                            <button onClick={()=> this.handleVoteUp(comment.comment_id)}>👍</button>
+                            <button onClick={()=> this.handleVoteDown(comment.comment_id)}>👎</button>
+                        </>
+                        )}
+                        {(user.username === comment.author ?
+                            <button className="delete-button" onClick={() =>this.deleteCommentHandler(comment.comment_id)}>❌</button> : '')}
+    
+                        <hr></hr>
+                    </div>
+                )
+            })
+        }
 
         return (
             <div className="comments-section">
                 <CommentAdder user={this.props.user} id={this.props.id}/>
-                {commentsToShow}
+                {commentsToShow ? commentsToShow : <h4>Loading content...</h4>}
             </div>
          );
     }
